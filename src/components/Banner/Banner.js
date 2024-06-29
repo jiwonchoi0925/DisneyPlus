dced55544cec6d63c4fb78658297d7fb539cd01a
jiwonchoi0925 <choijuwon0925@gmail.com>
2024-06-30 01:03:15 +0900

    topBar 스타일링, 상단에 나올 영화 정보 작업(api dats 가져오는 것만)

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
+import requests from '../../api/request';
 
 const Banner = () => {
+
+  const [movie, setMovie] =useState([]);
+
+  useEffect(()=> {
+    fatchData();
+  },[])
+
+  const fatchData= async()=>{
+    const nowPlaying = await axios.get(requests.fetchNowPlaying);
+
+    const movieId= nowPlaying.data.results[
+      Math.floor(Math.random() * nowPlaying.data.results.length)
+    ].id
+
+    const {data:movieDetail}=await axios.get(`movie/${movieId}`, {
+      params:{append_to_response:"videos"}
+    })
+    setMovie(movieDetail);
+  }
+
   return (
     <div>Banner</div>
   )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 const Navbar = () => {
+  const [show,setShow]=useState(false);
+
+  useEffect(()=>{
+    window.addEventListener('scroll',() => {
+      if(window.scrollY > 50){
+        setShow(true);
+      }else{
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    }
+  },[])
+
   return (
-    <div>Navbar</div>
+    <NavWrapper show={show}>
+      <Logo>
+        <img alt='DisneyPlus Logo'
+          src='/images/disney-logo.png' 
+          onClick={()=>window.location.href='/'}
+        />
+      </Logo>
+    </NavWrapper>
   )
 }
 
 export default Navbar
+
+const NavWrapper = styled.nav`
+  position:fixed;
+  top:0;
+  left:0;
+  right:0;
+  height:70px;
+  background-color: ${props => props.show ? '#090b13' : 'transparent'};
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index:3;
+`
+
+const Logo = styled.a`
+  padding:0;
+  width:80px;
+  margin-top:4px;
+  max-height:70px;
+  font-size:0;
+  display:inline-block;
+  img {
+    display:block;
+    width:100%;
+  }
+`
